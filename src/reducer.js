295bc82970e81ad3512e32faf9211d6ed7f4073b
Adnan Khan <adnan7.zain@gmail.com>
2021-11-28 22:15:57 +0500

    add individual cart item functioality

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,13 @@ const reducer = (state, action) => {
   if (action.type === 'CLEAR_CART') {
     return { ...state, cart: [] };
   }
+
+  if (action.type === 'REMOVE_ITEM') {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  }
 };
 
 export default reducer;
